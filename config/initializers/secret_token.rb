b5b738b9c67962f16dc02aa5131c81e560f01bde
Guillermo Guerrero <g.guerrero.bus@gmail.com>
2012-03-02 18:41:44 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirsApp::Application.config.secret_token = '86d10322f492e1a101d18fbcc87e99b43a03bc490e00f9437b33bb3d513ac9ecd911401600e84630bb0efeb9209a19b9e7a1ca818280e9f40af9e20e14dd8901'
